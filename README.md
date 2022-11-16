11be586302ec88e8ff1dab776e10c165b2b0bd65
judamar <juancho.o.mart@gmail.com>
2022-11-16 13:22:47 -0500

    añadiendo el @dc

--- README.md
+++ README.md
@@ -1,10 +1,19 @@
 # Beep
-## for 1.12.2
+## For 1.12.2
 
 Beeps when someone is mentioned in text with an `@`
 
-Leave a space before and after name when you use a @, for example `@ Lucun_Ji Hello!`
+Leave a space before and after name when you use a @, for example `@ judamar Hello!`
 
 `@ all ` represents all players in game
 
 Use two `@` for more powerful ping, for example `@@ all Hello!`
+
+## Comandos:
+
+``` 
+@ <name> -> 1 beep
+
+@@ <name> -> mensaje en pantalla diciendo que mire el chat y 3 beeps
+
+@dc <name> -> mensaje en pantalla diciendo que revise discord y 3 beeps```
--- beep/__init__.py
+++ beep/__init__.py
@@ -34,6 +34,28 @@ def beep_big(server: ServerInterface, info: Info, name_list):
 	beeeeeeep()
 
 
+def beep_dc(server: ServerInterface, info: Info, name_list):
+	if not name_list:
+		return
+	info.cancel_send_to_server()
+	source_name = RText(info.player if info.is_player else server.tr('beep.console'), RColor.red)
+	title: RTextBase = server.rtr('beep.title', source_name)
+	discord: RTextBase = server.rtr('beep.discord', source_name)
+
+	@new_thread('beepdc')
+	def beeeepdc():
+		for name in name_list:
+			with RTextMCDRTranslation.language_context(server.get_preference(name).language):
+				server.execute('title {} times 2 25 5'.format(name))
+				server.execute('title {} title {}'.format(name, title.to_json_str()))
+				server.execute('title {} subtitle {}'.format(name, discord.to_json_str()))
+		for i in range(3):
+			time.sleep(1.0 / 3)
+			beep_small(server, info, name_list)
+
+	beeeepdc()
+
+
 def find_name_list(text, pattern):
 	name_list = None
 	if text.find('{} '.format(pattern)) > -1:
@@ -50,7 +72,8 @@ def on_user_info(server: ServerInterface, info: Info):
 	if info.content.find('@ ') > -1:
 		beep_small(server, info, find_name_list(info.content, '@'))
 		beep_big(server, info, find_name_list(info.content, '@@'))
-		
+	elif info.content.find('@dc ') > -1:
+		beep_dc(server, info, find_name_list(info.content, '@dc'))
 
 def on_load(server: PluginServerInterface, old):
 	server.register_help_message('@ <someone>', server.get_self_metadata().description)
